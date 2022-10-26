bf2c6e82769a62f321001bc8f3b6532755606963
Prasanth Kumar Gollapudi <Gollapudi.PrasanthKumar@lntinfotech.com>
2022-10-26 17:31:04 +0530

    added custom prop

--- src/main/java/com/schindler/demo/controllers/ViewControlller.java
+++ src/main/java/com/schindler/demo/controllers/ViewControlller.java
@@ -3,6 +3,7 @@ package com.schindler.demo.controllers;
 import com.schindler.demo.model.Rna;
 import com.schindler.demo.service.RnaServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,13 @@ public class ViewControlller {
     @Autowired
     private RnaServiceImpl rnaService;
 
+    @Value("${spring.custom.prop}")
+    private String valueFromProp;
+
+    @GetMapping("/")
+    public String getValue(){
+        return valueFromProp;
+    }
 
 
     @GetMapping("/{id}")
